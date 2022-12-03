99a71c538e88ac2b0a9ba30ccf30d3b7c4e595f0
Guy Marshall <guymarshall9@gmail.com>
2022-12-03 22:14:53 +0000

    Rename pyramid() to square_pyramid()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn pyramid(number: i32) -> i32 {
+fn square_pyramid(number: i32) -> i32 {
     number * (number + 1) * ((2 * number) + 1) / 6
 }
 
@@ -9,7 +9,7 @@ fn square(number: i32) -> i32 {
 fn main() {
     'outer: for i in 4..100 {
         for j in 4..100 {
-            let pyramid_result: i32 = pyramid(i);
+            let pyramid_result: i32 = square_pyramid(i);
             let square_result: i32 = square(j);
 
             println!("{}:{} - pyramid: {}, square: {}", i, j, pyramid_result, square_result);
