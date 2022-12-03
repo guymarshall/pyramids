4efb30bdffad85d83abb30737c4122fc76ea3549
Guy Marshall <guymarshall9@gmail.com>
2022-12-03 22:06:15 +0000

    Add basic formulas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let k: i32 = 24;
+    let n: i32 = 70;
+
+    let pyramid: i32 = k * (k + 1) * ((2 * k) + 1) / 6;
+    let square:i32 = n * n;
+
+    println!("pyramid: {}, square: {}", pyramid, square);
 }
